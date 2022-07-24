c69ea57bba7b781cb738f1b4fbf3890767205273
Gabriel Damian <gabbydamian92@gmail.com>
2022-07-24 18:53:47 +0100

    item category pages

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,38 @@
-fetch ("../../treatments.json")
-.then(response => {
-    return response.json();
-})
-.then(data => 
-    console.log(data)
-);
+// getTreatments();
+// async function getTreatments() {
+//   let response = await fetch('../../treatments.json');
+//   let data = await response.json();
+//   // console.log(data.treatments);
+//   let [injectables, skin, body] = data.treatments;
+
+//   let {
+//     injectablesId,
+//     injectablesName,
+//     injectablesDescription,
+//     injectablesImage,
+//     injectablesTreatments,
+//   } = injectables;
+//   let { skinId, skinName, skinDescription, skinImage, skinTreatments } = skin;
+//   let { bodyId, bodyName, bodyDescription, bodyImage, bodyTreatments } = body;
+
+//   let [nose, chin, eyeBags] = injectablesTreatments;
+//   let [acne, ageSpots, melasma] = skinTreatments;
+//   let [cellulite, stretchMarks, skinTags] = bodyTreatments;
+
+//   let {
+//     noseId,
+//     noseName,
+//     noseShort,
+//     noseLong,
+//     noseImage,
+//     noseTime,
+//     noseDowntime,
+//     noseDuration,
+//     noseProcedure,
+//     noseResults,
+//     noseFaq,
+//   } = nose;
+//   console.log(injectablesTreatments);
+// }
+
+// //Destructuring is incomplete
